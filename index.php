1a23cba296046fdb68b4762fb375e969b23e5ab2
unknown <zakyx1ggg@gmail.com>
2025-01-07 09:16:05 +0800

    selesai

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+    //simpan nama dan nisn kalian dalam variable
+    $nama = "Ahmad Zaky Ramadhan";
+    $nisn = "0073242345";
+    //tampilkan dan cetak variable kedalam layar
+    echo 'Nama :', $nama;
+    echo '<br>' ;
+    echo 'NISN :', $nisn;
 
 ?>
